ba78f506433b966302bcb54900c5c66ec28e0d72
Quoc <LamQuoc.24@gmail.com>
2022-05-13 16:31:52 -0400

    Dividers

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+//! -------------------------------- Variables ------------------------------- */
+
 let input = document.querySelector("#input");
+let toGuess = document.querySelector("#word-button");
 
-
-for (let i = 0; i< 27; i++){
-    document.createElement("div");
-    
-
+for (let i = 0; i < 27; i++) {
+  document.createElement("div");
 }
+
