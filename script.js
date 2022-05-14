c056074bc22f33433008db1624ae921f54a2322f
Quoc <LamQuoc.24@gmail.com>
2022-05-14 19:36:38 -0400

    Made Keyboard

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ for (let i = 0; i < 27; i++) {
   document.createElement("div");
 }
 
+//! ----------------------------- Keyboard Mockup ---------------------------- */
+
+let keyboard = document.querySelector("#keyboard");
+
+for (let k = 0; k < 26; k++) {
+  let key = document.createElement("div");
+  key.classList.add("key");
+  key.id = k;
+  key.innerText = k;
+  key.addEventListener('click', ()=>{
+                 //!The innerText is compared to word array
+  })
+  keyboard.append(key);
+}
